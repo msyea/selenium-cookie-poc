f0ca2b4f36543027b31ae2610f4ac67428b49868
Simon Mayes <simon.mayes@bglgroup.co.uk>
2018-12-19 10:59:23 +0000

    Setup test runner and docker

---
+++ test/some-random-test.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('selenium playing well with certificates', () => {
+  it ('should load a secure page without crying', () => {
+    assert.strictEqual(true, false)
+  })
+})
